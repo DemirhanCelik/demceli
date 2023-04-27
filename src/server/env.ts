bb9011b866552f4cdcf2fff7ab659a366b353c27
Demirhan <demirhanceliik@gmail.com>
2023-04-27 09:24:18 +0300

    Update env.ts

--- src/server/env.ts
+++ src/server/env.ts
@@ -1,12 +1,33 @@
-import {envsafe, str, url} from 'envsafe';
+// import {envsafe, str, url} from 'envsafe';
 
-export const env = envsafe({
-	APPLE_TEAM_ID: str(),
-	APPLE_KEY_ID: str(),
-	APPLE_PRIV_KEY: str(),
-	DISCORD_WEBHOOK: url(),
-	TURNSTILE_SECRET_KEY: str(),
-	DEFAULT_LOCATION: str({
-		default: 'London, UK',
-	}),
-});
+// export const env = envsafe({
+// 	APPLE_TEAM_ID: str({
+// 		default: 'London, UK',
+// 	}),
+// 	APPLE_KEY_ID: str({
+// 		default: 'London, UK',
+// 	}),
+// 	APPLE_PRIV_KEY: str({
+// 		default: 'London, UK',
+// 	}),
+// 	DISCORD_WEBHOOK: url({
+// 		default: 'https://google.com',
+// 	}),
+// 	TURNSTILE_SECRET_KEY: str({
+// 		default: 'London, UK',
+// 	}),
+// 	DEFAULT_LOCATION: str({
+// 		default: 'London, UK',
+// 	}),
+// });
+
+export const env = {
+	"APPLE_TEAM_ID":"mehmet",
+	"APPLE_KEY_ID":"ahmet",
+	"APPLE_PRIV_KEY":"veli",
+	"DISCORD_WEBHOOK":"https://google.com",
+	"TURNSTILE_SECRET_KEY":"veli",
+	"DEFAULT_LOCATION":"ali"
+}
+
+
