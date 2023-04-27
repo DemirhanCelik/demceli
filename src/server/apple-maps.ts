f26cc5a878e5ca7e1a1bd86b42ae5453907c503a
Demirhan <demirhanceliik@gmail.com>
2023-04-27 09:30:51 +0300

    Update apple-maps.ts

--- src/server/apple-maps.ts
+++ src/server/apple-maps.ts
@@ -1,24 +1,27 @@
-import jwa from 'jwa';
-import {env} from './env';
+// import jwa from 'jwa';
+// import {env} from './env';
 
-const es256 = jwa('ES256');
+// const es256 = jwa('ES256');
 
-export function getMapURL(center: string) {
-	const params = new URLSearchParams({
-		center,
-		teamId: env.APPLE_TEAM_ID,
-		keyId: env.APPLE_KEY_ID,
-		z: '13',
-		colorScheme: 'dark',
-		size: '340x200',
-		scale: '2',
-		t: 'mutedStandard',
-		poi: '0',
-	});
 
-	const completePath = `/api/v1/snapshot?${params.toString()}`;
+export const ali = "https://google.com"
 
-	const signature = es256.sign(completePath, env.APPLE_PRIV_KEY);
+// export function getMapURL(center: string) {
+// 	const params = new URLSearchParams({
+// 		center,
+// 		teamId: env.APPLE_TEAM_ID,
+// 		keyId: env.APPLE_KEY_ID,
+// 		z: '13',
+// 		colorScheme: 'dark',
+// 		size: '340x200',
+// 		scale: '2',
+// 		t: 'mutedStandard',
+// 		poi: '0',
+// 	});
 
-	return `https://snapshot.apple-mapkit.com${completePath}&signature=${signature}`;
-}
+// 	const completePath = `/api/v1/snapshot?${params.toString()}`;
+
+// 	const signature = es256.sign(completePath, env.APPLE_PRIV_KEY);
+
+// 	return `https://snapshot.apple-mapkit.com${completePath}&signature=${signature}`;
+// }
